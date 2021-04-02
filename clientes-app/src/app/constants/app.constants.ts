7bfe1d16832997083fa364a0981bd24d409eda89
alex <alex.olucas@hotmail.com>
2021-04-02 18:15:18 -0300

    atualização

--- clientes-app/src/app/constants/app.constants.ts
+++ clientes-app/src/app/constants/app.constants.ts
@@ -6,6 +6,6 @@ export class AppConstants {
     private static OAUTH2_URL = environment.API_BASE_URL + "/oauth2/authorization/";
     private static REDIRECT_URL = "?redirect_uri=http://localhost:4200/login";
     public static API_URL = environment.API_BASE_URL + "/api/";
-    public static AUTH_API = AppConstants.API_URL + "/auth/";
+    public static AUTH_API = AppConstants.API_URL + "auth/";
     public static GOOGLE_AUTH_URL = AppConstants.OAUTH2_URL + "google" + AppConstants.REDIRECT_URL;
 }
