13853b55fcfd1a77a879c2785c8bf57a27a3adf0
alex <alex.olucas@hotmail.com>
2021-04-06 17:14:23 -0300

    login

--- backend/src/main/java/com/projeto/servicos/model/controller/ApplicationControllerAdvice.java
+++ backend/src/main/java/com/projeto/servicos/model/controller/ApplicationControllerAdvice.java
@@ -17,7 +17,7 @@ import com.projeto.servicos.model.controller.exception.ApiErrors;
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
 
-	/*@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ApiErrors handleValidationErros(MethodArgumentNotValidException ex) {
 
@@ -27,7 +27,7 @@ public class ApplicationControllerAdvice {
 
 		return new ApiErrors(messages);
 
-	}*/
+	}
 
 	@ExceptionHandler(ResponseStatusException.class)	
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -42,16 +42,5 @@ public class ApplicationControllerAdvice {
 	}
 	
 	
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ResponseEntity<ApiErrors> handleValidationErros(ResponseStatusException ex) {
-
-		
-		String mensagemErro = ex.getReason();
-		HttpStatus codigoStatus = ex.getStatus();
-		ApiErrors apiErrors = new ApiErrors(mensagemErro);
-		return new ResponseEntity<ApiErrors>(apiErrors, codigoStatus);
-
-	}
 
 }
