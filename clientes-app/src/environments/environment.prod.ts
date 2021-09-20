2c92acc437a2057b471cb6bf5fcb28ac15a2002a
alexozalucas <alex.olucas@hotmail.com>
2021-09-20 20:15:02 -0300

    ajustes finais

--- clientes-app/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-
-  production: true,
-  API_BASE_URL: 'https://servicosprestados.herokuapp.com', 
-  API_URL : "https://servicosprestados.herokuapp.com/api/",
-  AUTH_API : "https://servicosprestados.herokuapp.com/api/auth/",
-  GOOGLE_AUTH_URL : "https://servicosprestados.herokuapp.com/oauth2/authorization/google?redirect_uri=https://prestacaoservicos.netlify.app"
-  
-};
