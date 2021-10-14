456603eaac28d192833c615f0fb3178d6e9331ab
alexozalucas <alex.olucas@hotmail.com>
2021-10-14 20:40:16 -0300

    ultimos ajustes front

--- clientes-app/src/environments/environment.ts
+++ clientes-app/src/environments/environment.ts
@@ -1,16 +1,16 @@
 export const environment = {
 
-  production: false,
-  API_BASE_URL: 'http://localhost:8080',
-  API_URL: "http://localhost:8080/api/",
-  AUTH_API: "http://localhost:8080/api/auth/",
-  GOOGLE_AUTH_URL: "http://localhost:8080/oauth2/authorization/google?redirect_uri=http://localhost:4200/login",
-  //production: true,
-  //API_BASE_URL: 'https://servicosprestados.herokuapp.com',
-  //API_URL: "https://servicosprestados.herokuapp.com/api/",
-  //AUTH_API: "https://servicosprestados.herokuapp.com/api/auth/",
-  //GOOGLE_AUTH_URL: "https://servicosprestados.herokuapp.com/oauth2/authorization/google?redirect_uri=https://prestacaoservicos.netlify.app/login",
-  //GOOGLE_AUTH_URL: "http://localhost:8080/oauth2/authorization/google?redirect_uri=https://prestacaoservicos.netlify.app",
+ // production: false,
+ // API_BASE_URL: 'http://localhost:8080',
+ // API_URL: "http://localhost:8080/api/",
+ // AUTH_API: "http://localhost:8080/api/auth/",
+ // GOOGLE_AUTH_URL: "http://localhost:8080/oauth2/authorization/google?redirect_uri=http://localhost:4200/login",
+ production: true,
+ API_BASE_URL: 'https://servicosprestados.herokuapp.com',
+ API_URL: "https://servicosprestados.herokuapp.com/api/",
+ AUTH_API: "https://servicosprestados.herokuapp.com/api/auth/",
+ GOOGLE_AUTH_URL: "https://servicosprestados.herokuapp.com/oauth2/authorization/google?redirect_uri=https://prestacaoservicos.netlify.app/login",
+ 
 
 
 
